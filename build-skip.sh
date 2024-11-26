c73ac85fa46f73f8e431f33accaaef4be5e67012
Adam <adamjamesturner93@gmail.com>
2024-11-26 06:35:14 +0000

    build script update

--- build-skip.sh
+++ build-skip.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] && [[ "$VERCEL_GITHUB_DEPLOYMENT" == "1" ]]; then
+echo "Debug: VERCEL_GIT_COMMIT_REF=$VERCEL_GIT_COMMIT_REF"
+echo "Debug: VERCEL_GITHUB_DEPLOYMENT=$VERCEL_GITHUB_DEPLOYMENT"
+echo "Debug: VERCEL_URL=$VERCEL_URL"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] && [[ -n "$VERCEL_GITHUB_DEPLOYMENT" ]]; then
   echo "Building main branch from GitHub Actions trigger"
+  exit 1
+elif [[ -n "$VERCEL_URL" ]]; then
+  echo "Building from deploy hook"
   exit 1
 else
   echo "Skipping build"
