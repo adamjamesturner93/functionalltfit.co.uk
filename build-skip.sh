1a9426c72008ec1e8a01eda7d6efdfb434542c16
Adam <adamjamesturner93@gmail.com>
2024-11-26 06:42:52 +0000

    small tweak

--- build-skip.sh
+++ build-skip.sh
@@ -3,12 +3,11 @@
 echo "Debug: VERCEL_GIT_COMMIT_REF=$VERCEL_GIT_COMMIT_REF"
 echo "Debug: VERCEL_GITHUB_DEPLOYMENT=$VERCEL_GITHUB_DEPLOYMENT"
 echo "Debug: VERCEL_URL=$VERCEL_URL"
+echo "Debug: VERCEL_GIT_COMMIT_AUTHOR_LOGIN=$VERCEL_GIT_COMMIT_AUTHOR_LOGIN"
+echo "Debug: VERCEL_GIT_COMMIT_MESSAGE=$VERCEL_GIT_COMMIT_MESSAGE"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] && [[ -n "$VERCEL_GITHUB_DEPLOYMENT" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] && [[ "$VERCEL_GIT_COMMIT_MESSAGE" == *"[deploy]"* ]]; then
   echo "Building main branch from GitHub Actions trigger"
-  exit 1
-elif [[ -n "$VERCEL_URL" ]]; then
-  echo "Building from deploy hook"
   exit 1
 else
   echo "Skipping build"
